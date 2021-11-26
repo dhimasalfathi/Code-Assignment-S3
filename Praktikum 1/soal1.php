7ca8c357a4cf3b2e28929c3af75a2569e324132f
dhimasalfathi <vitosmart2@gmail.com>
2021-11-26 07:27:00 +0700

    Praktikum 1

---
+++ Praktikum 1/soal1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+  $brush_price = 5; 
+
+  echo "<table border=\"1\" align=\"center\">";
+  echo "<tr><th>Quantity</th>";
+  echo "<th>Price</th></tr>";
+
+  for ( $counter = 10; $counter <= 100; $counter += 5) {
+    echo "<tr><td>";
+    echo $counter;
+    echo "</td><td>";
+    echo $brush_price * $counter;
+    echo "</td></tr>";
+  }
+
+  echo "</table>";
+
+?>
+
+</body>
+</html>
