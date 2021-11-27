5838feaea54dd4ba5bd514d8881dad50a5b31164
dhimasalfathi <vitosmart2@gmail.com>
2021-11-27 11:55:09 +0700

    Praktikum 4

---
+++ Praktikum 4/tugas1.php
@@ -0,0 +1,7 @@
+<?php
+$hari = ['Sen' => 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu', 'Minggu'];
+
+foreach ($hari as $key => $value) {
+    echo " $key is $value, ";
+  }
+?>
---
+++ Praktikum 4/tugas2.php
@@ -0,0 +1,12 @@
+<?php
+    $info = [
+    'komputer mekanik' => 'Charles Babbage',
+    'world wide web' => 'Tim Berners-Lee',
+    'mesin uap' => 'James Watt'
+  ];
+
+  foreach ($info as $key => $value) {
+    echo " $key menemukan  $value, <br> ";
+  }
+
+?>
