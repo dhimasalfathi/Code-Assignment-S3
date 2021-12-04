f83d6201c2da09cac2ab347226b4e57add6b9396
dhimasalfathi <vitosmart2@gmail.com>
2021-12-04 11:35:40 +0700

    Praktikum 7

---
+++ Praktikum 7/soal5.php
@@ -0,0 +1,13 @@
+<html>
+
+<body>
+
+    <form action="soal6get.php" method="post">
+        Name: <input type="text" name="name"><br>
+        Comment: <input type="text" name="comment"><br>
+        <input type="submit">
+    </form>
+
+</body>
+
+</html>
