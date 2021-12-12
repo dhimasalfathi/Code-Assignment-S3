a0695682b2d440cd5b654090a052aeb46d85e7a2
dhimasalfathi <vitosmart2@gmail.com>
2021-12-12 20:09:00 +0700

    PAW

---
+++ Test/koneksiMVC.php
@@ -0,0 +1,11 @@
+<?php
+class koneksiMVC
+{
+
+    public $mysqli;
+
+    public function __construct()
+    {
+        $this->mysqli =  mysqli_connect("localhost", "root", "gondes", "praktikum12");
+    }
+}
